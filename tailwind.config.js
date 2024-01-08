65468bb2495300c20d1084b2f38070eb8f7061f3
deepak-raaaz <deepakjamui26@gmail.com>
2024-01-08 19:25:07 +0530

    home page updated

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                brand: {
+                    "50": "#FFEFE9",
+                    "100": "#FFCEBC",
+                    "200": "#FFCEBC",
+                    "300": "#FFB69B",
+                    "400": "#FF946E",
+                    "500-main": "#FF6026",
+                    "600": "#E85723",
+                    "700": "#B5441B",
+                    "800": "#8C3515",
+                    "900": "#6B2810"
+                }
+            },
+            fontFamily: {
+                poppins : ['Poppins', 'sans-serif']
+            }
+        }
+    }
+  }
